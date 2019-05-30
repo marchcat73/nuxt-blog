53f0df450d9fd21d1b7bb4b11df3da4c0865ed63
unknown <D:\Program Files (x86)\The Bat!\Mail>
2019-05-30 22:57:35 +0400

    commit 2

---
+++ components/main/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <article class="post">
+
+  </article>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    height: 200px;
+    border: 1px solid #000;
+    margin-bottom: 2rem;
+  }
+</style>
+
+

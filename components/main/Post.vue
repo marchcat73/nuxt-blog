98bdb0898fba1d749d8d11497ecfd17ad68b7b15
unknown <D:\Program Files (x86)\The Bat!\Mail>
2019-06-01 01:23:43 +0400

    commit

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,20 +1,68 @@
 <template>
   <article class="post">
-
+    <el-card
+      shadow="hover"
+      :body-style="{padding: 0}"
+      class="post"
+    >
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post-body">
+      <img src="http://www.go2festival.com/wp-content/uploads/2016/04/berlin-4.jpg" alt="" class="post-img">
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">Open</el-button>
+      <span>
+        <i class="el-icon-message"></i>
+        12
+      </span>
+    </footer>
+    </el-card>
   </article>
 </template>
 
 <script>
 export default {
-  
+  methods: {
+    openPost() {
+      const id = 'test-id'
+      this.$router.push(`/post/${id}`)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   .post {
-    height: 200px;
-    border: 1px solid #000;
     margin-bottom: 2rem;
+  }
+
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .post-img {
+    max-width: 100%;
+    width: 100%;
+    height: auto;
+  }
+
+  .post-body {
+    
+  }
+
+  .post-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
   }
 </style>
 
---
+++ pages/post/_id.vue
@@ -0,0 +1,79 @@
+<template>
+  <article class="post">
+    <header class="post-header">
+      <div class="post-title">
+        <h1>Post title</h1>
+        <nuxt-link to="/">
+          <i class="el-icon-back"></i>
+        </nuxt-link>
+      </div>
+      <div class="post-info">
+        <small>
+          <i class="el-icon-time"></i>
+          {{ new Date().toLocaleString() }}
+        </small>
+        <small>
+          <i class="el-icon-view"></i>
+          42
+        </small>
+      </div>
+      <div class="post-image">
+        <img src="http://www.go2festival.com/wp-content/uploads/2016/04/berlin-4.jpg" alt="">
+      </div>
+    </header>
+    <main class="post-content">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, eius voluptas tempore nostrum provident reprehenderit delectus totam ullam, accusamus cum recusandae asperiores labore blanditiis quisquam fugit soluta quasi! Labore, nam.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga iusto suscipit veritatis ex et perspiciatis error eum! Vel quas a, tempore totam odit reiciendis tenetur repellat illum asperiores explicabo blanditiis!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit labore quasi eaque facere aperiam ipsum, autem voluptate fuga nemo optio error, maiores iusto reprehenderit corrupti vero at beatae neque, itaque distinctio? Officiis, sed blanditiis quam at voluptatem dignissimos, voluptate fugit, pariatur perferendis iusto vero repellendus ipsa cumque consectetur facilis!</p>
+    </main>
+    <footer>
+
+    </footer>
+  </article>
+</template>
+
+<script>
+export default {
+  validate({params}) {
+    return Boolean(params.id)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  .post-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+  .post-info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: .5rem;   
+  }
+  .post-image {
+    img {
+      width: 100%;
+      max-width: 100%;
+      height: auto;
+    }
+  }
+  .post-header {
+    margin-bottom: 1.5rem;
+  }
+  .post-content {
+    margin-bottom: 2rem;
+
+    p {
+      margin-bottom: 0.5rem;
+    }
+  }
+</style>
+
+

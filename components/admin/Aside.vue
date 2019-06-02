6680e172aa3b75439bffca5ab1ff579a06fe01f2
unknown <D:\Program Files (x86)\The Bat!\Mail>
2019-06-02 09:17:54 +0400

    commit

---
+++ components/admin/Aside.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-menu
+    router
+    :default-active="$route.path"
+    class="admin-menu"
+  >
+    <el-menu-item index="/admin">
+      <i class="el-icon-menu"></i>
+      <span>Аналитика</span>
+    </el-menu-item>
+    <el-menu-item index="/admin/create">
+      <i class="el-icon-edit"></i>
+      <span>Создать</span>
+    </el-menu-item>
+    <el-menu-item index="/admin/list">
+      <i class="el-icon-tickets"></i>
+      <span>Посты</span>
+    </el-menu-item>
+    <el-menu-item index="/admin/user">
+      <i class="el-icon-s-custom"></i>
+      <span>Пользователи</span>
+    </el-menu-item>
+    <el-menu-item index="/admin/logout">
+      <i class="el-icon-d-arrow-left"></i>
+      <span>Выйти</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style lang="scss" scoped>
+  .admin-menu {
+    height: 100%;
+  }
+</style>
+
---
+++ pages/admin/logout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    logout
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'admin'
+}
+</script>

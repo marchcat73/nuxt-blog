9cd628de7578a0a97fbb908e409ac17769cd22cf
unknown <D:\Program Files (x86)\The Bat!\Mail>
2019-06-08 18:36:03 +0400

    commit

---
+++ store/auth.js
@@ -0,0 +1,29 @@
+export const state = () => ({
+  token: null
+})
+
+export const mutations = {
+  
+  setToken(state, token) {
+    state.token = token
+  }
+}
+
+export const actions = {
+  async login({commit, dispatch}, formData) {
+    const token = await new Promise(resolve => {
+      setTimeout(() => resolve('mock-token'), 200)
+    })
+    
+    dispatch('setToken', token)
+  },
+
+  setToken({commit}, token) {
+    
+    commit('setToken', token)
+  }
+}
+
+export const getters = {
+  isAuthenticated: state => Boolean(state.token)
+}

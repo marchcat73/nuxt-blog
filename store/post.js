89e47eddebbda3583f95bdde8496977c9112bfe4
Vitaliy Vanyushin <v.vanyushin@pro100agency.ru>
2019-06-11 14:32:54 +0400

    commit 001

---
+++ store/post.js
@@ -0,0 +1,16 @@
+export const state = () => ({
+
+})
+
+export const actions = {
+	async fetchAdmin({}) {
+		return new Promise(resolve => {
+			setTimeout(() => {
+				resolve([
+					{title: 'Post 1', date: new Date(), views: 22, comments: [1, 2], _id: Math.random()},
+					{title: 'Post 2', date: new Date(), views: 15, comments: [1, 2], _id: Math.random()}
+				])
+			}, 1000)
+		})
+	}
+}

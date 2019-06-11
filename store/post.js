a54309688c510ed51f8d399ee2ff292fe8abd414
Vitaliy Vanyushin <v.vanyushin@pro100agency.ru>
2019-06-11 16:47:19 +0400

    commit br 001

--- store/post.js
+++ store/post.js
@@ -1,3 +1,8 @@
+const posts = [
+	{title: 'Post 1', date: new Date(), views: 22, comments: [1, 2], _id: 'id1'},
+	{title: 'Post 2', date: new Date(), views: 15, comments: [1, 2], _id: 'id2'}
+]
+
 export const state = () => ({
 
 })
@@ -6,15 +11,24 @@ export const actions = {
 	async fetchAdmin({}) {
 		return new Promise(resolve => {
 			setTimeout(() => {
-				resolve([
-					{title: 'Post 1', date: new Date(), views: 22, comments: [1, 2], _id: Math.random()},
-					{title: 'Post 2', date: new Date(), views: 15, comments: [1, 2], _id: Math.random()}
-				])
+				resolve(posts)
 			}, 1000)
 		})
 	},
 
 	async remove({}, id) {
-		
+
+  },
+  
+	async update({}, {id, text}) {
+
+	},
+
+	async fetchAdminById({}, id) {
+		return new Promise(resolve => {
+			setTimeout(() => {
+				resolve(posts.find(p => p._id === id))
+			}, 1000)
+		})
 	}
 }

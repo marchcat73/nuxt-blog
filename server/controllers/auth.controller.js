0adb3174e74676378d2ea5090b5297c760b3c17c
Vitaliy Vanyushin <v.vanyushin@pro100agency.ru>
2019-06-14 16:30:08 +0400

    commit

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -24,6 +24,20 @@ module.exports.login = async (req, res) => {
   }
 }
 
-module.exports.createUser = (req, res) => {
+module.exports.createUser = async (req, res) => {
+  const candidate = await User.findOne({login: req.body.login})
 
+  if (candidate) {
+    res.status(409).json({message: 'Логин занят'})
+  } else {
+    const salt = bcrypt.genSaltSync(10)
+    const user = new User({
+      login: req.body.login,
+      password: bcrypt.hashSync(req.body.password, salt)
+    })
+
+    await user.save()
+    // console.log(user._id)
+    res.status(201).json(user)
+  }
 }
